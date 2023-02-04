17b57d7659c021af61bfb52e70816f41889f729a
Monkifoto LLc <monkifoto@gmail.com>
2023-02-03 23:51:10 -0800

    added real estate headshots inquire links

--- monkifoto-web/src/app/app-routing.module.ts
+++ monkifoto-web/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeadshotsComponent } from './headshots/headshots.component';
 import { HomeComponent } from './home/home.component';
+import { InquireComponent } from './inquire/inquire.component';
+import { RealEstateComponent } from './real-estate/real-estate.component';
 import { WeddingGalleryComponent } from './wedding-gallery/wedding-gallery.component';
 
 const routes: Routes = [
   {path:'wedding-gallery', component: WeddingGalleryComponent},
+  {path:'real-estate', component: RealEstateComponent},
+  {path:'inqure', component:InquireComponent},
+  {path:'headshots', component: HeadshotsComponent},
   {path:'home', component: HomeComponent}
 ];
 
