549ccceabe538b0a23833d8f9809539651ab0387
Monkifoto LLc <monkifoto@gmail.com>
2023-02-03 23:42:52 -0800

    added wedding gallery and home

---
+++ monkifoto-web/src/app/wedding-gallery/wedding-gallery.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wedding-gallery',
+  templateUrl: './wedding-gallery.component.html',
+  styleUrls: ['./wedding-gallery.component.scss']
+})
+export class WeddingGalleryComponent {
+
+}
