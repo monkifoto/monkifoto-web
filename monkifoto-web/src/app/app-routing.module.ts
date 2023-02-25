f60c0dd5d77334305c40112a08761812214bf4ab
Monkifoto LLc <monkifoto@gmail.com>
2023-02-24 22:33:06 -0800

    changes

--- monkifoto-web/src/app/app-routing.module.ts
+++ monkifoto-web/src/app/app-routing.module.ts
@@ -1,18 +1,34 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeadshotsComponent } from './cat-portraits/headshots/headshots.component';
+
 import { HomeComponent } from './home/home.component';
 import { InquireComponent } from './inquire/inquire.component';
-import { RealEstateComponent } from './cat-architecture/real-estate/real-estate.component';
 import { PortfolioComponent } from './split-page/portfolio/portfolio.component';
+
+//Events
 import { WeddingGalleryComponent } from './cat-events/wedding-gallery/wedding-gallery.component';
+import { EngagmentsComponent } from './cat-events/engagments/engagments.component';
+import { CorporateComponent } from './cat-events/corporate/corporate.component';
+import { EventsComponent } from './cat-events/events/events.component';
+//Portrait
+import { HeadshotsComponent } from './cat-portraits/headshots/headshots.component';
 import { SeniorPortraitsComponent } from './cat-portraits/senior-portraits/senior-portraits.component';
+import { FamilyComponent } from './cat-portraits/family/family.component';
+//Studio
 import { StudioComponent } from './cat-studio/studio/studio.component';
 import { ProductComponent } from './cat-studio/product/product.component';
+//Architecture
+import { RealEstateComponent } from './cat-architecture/real-estate/real-estate.component';
+//Personal
+
 
 const routes: Routes = [
   {path:'', component:HomeComponent}, // Default page
   {path:'wedding-gallery', component: WeddingGalleryComponent},
+  {path:'engagment', component: EngagmentsComponent},
+  {path:'corporate', component:CorporateComponent},
+  {path: 'events', component: EventsComponent},
+  {path: 'family', component: FamilyComponent},
   {path: 'senior', component: SeniorPortraitsComponent},
   {path:'real-estate', component: RealEstateComponent},
   {path:'inquire', component:InquireComponent},
